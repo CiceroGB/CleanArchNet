98b978573e88b3fb52dfbffcefaf26f41f87f9b9
CiceroGB <brunelli.cg@gmail.com>
2023-07-15 16:02:08 -0300

    feat: anemic classes

---
+++ CleanArchNet.Domain/Entities/Category.cs
@@ -0,0 +1,12 @@
+namespace CleanArchNet.Domain.Entities;
+
+public class Category
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public ICollection<Product> Products { get; set; }
+
+
+}
---
+++ CleanArchNet.Domain/Entities/Product.cs
@@ -0,0 +1,18 @@
+using CleanArchNet.Domain.Entities;
+
+namespace CleanArchNet.Domain;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public int Stock { get; set; }
+    public string Image { get; set; }
+
+    public int IdCategory { get; set; }
+
+    public Category Category { get; set; }
+
+}
