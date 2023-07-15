153c15e166b28912f3d04f47a5abb46653338bc1
CiceroGB <brunelli.cg@gmail.com>
2023-07-15 16:22:23 -0300

    feat: business rules

--- CleanArchNet.Domain/Entities/Category.cs
+++ CleanArchNet.Domain/Entities/Category.cs
@@ -1,12 +1,37 @@
 namespace CleanArchNet.Domain.Entities;
 
-public class Category
-{
-    public int Id { get; set; }
+using CleanArchNet.Domain.Validation;
 
-    public string Name { get; set; }
+public sealed class Category
+{
+    public int Id { get; private set; }
+
+    public string Name { get; private set; }
+
+    public Category(string name)
+    {
+        ValidateDomain(name);
+
+    }
+
+    public Category(int id, string name)
+    {
+        DomainExceptionValidation.When(id < 0, "Invalid Id value");
+        Id = id;
+        ValidateDomain(name);
+
+    }
 
     public ICollection<Product> Products { get; set; }
+
+
+    private void ValidateDomain(string name)
+    {
+        DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid - input is required");
+        DomainExceptionValidation.When(name.Length < 3, "Invalid - input is too short");
+
+        Name = name;
+    }
 
 
 }
