6dcac77c595c2deb9f865a92ba75defa7564936a
CiceroGB <brunelli.cg@gmail.com>
2023-07-15 22:05:02 -0300

    feat: repositories

---
+++ CleanArchNet.Domain/Repositories/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+using CleanArchNet.Domain.Entities;
+
+namespace CleanArchNet.Domain.Repositories;
+
+
+public interface ICategoryRepository
+{
+    Task<IEnumerable<Category>> GetCategoriesAsync();
+    Task<Category> GetByIdAsync(int? id);
+    Task<Category> CreateAsync(Category category);
+    Task<Category> UpdateAsync(Category category);
+    Task<Category> RemoveAsync(Category category);
+
+}
