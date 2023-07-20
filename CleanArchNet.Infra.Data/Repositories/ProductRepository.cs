614649cf0f26f62a56bb85f787e4cc41de5a59b5
CiceroGB <brunelli.cg@gmail.com>
2023-07-20 12:05:30 -0300

    feat: infra ioc

---
+++ CleanArchNet.Infra.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,55 @@
+using CleanArchNet.Domain.Entities;
+using CleanArchNet.Domain.Repositories;
+using CleanArchNet.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace CleanArchNet.Infra.Data.Repositories;
+
+public class ProductRepository : IProductRepository
+{
+    ApplicationDbContext _productContext;
+    public ProductRepository(ApplicationDbContext context)
+    {
+        _productContext = context;
+    }
+
+    public async Task<Product> CreateAsync(Product product)
+    {
+        _productContext.Add(product);
+        await _productContext.SaveChangesAsync();
+        return product;
+    }
+
+    public async Task<Product> GetByIdAsync(int? id)
+    {
+        //return await _productContext.Products.FindAsync(id);
+        return await _productContext.Products.Include(c => c.Category)
+            .SingleOrDefaultAsync(p => p.Id == id);
+    }
+
+    public async Task<Product> GetProductCategoryAsync(int? id)
+    {
+        return await _productContext.Products.Include(c => c.Category)
+            .SingleOrDefaultAsync(p => p.Id == id);
+    }
+
+    public async Task<IEnumerable<Product>> GetProductsAsync()
+    {
+        return await _productContext.Products.ToListAsync();
+    }
+
+    public async Task<Product> RemoveAsync(Product product)
+    {
+        _productContext.Remove(product);
+        await _productContext.SaveChangesAsync();
+        return product;
+    }
+
+    public async Task<Product> UpdateAsync(Product product)
+    {
+        _productContext.Update(product);
+        await _productContext.SaveChangesAsync();
+        return product;
+    }
+}
